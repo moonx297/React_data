0530e1c83b7e0c5c44d02e2551d5e7fdda3684fa
DESKTOP-HJL050Q\ez220409 <moonx297@crk.umn.edu>
2022-08-10 18:24:59 +0900

    리액트 연습_081022

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,16 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
-// import notify from './notify';   
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import loadable from '@loadable/component';
+const SplitMe = loadable(() => import('./SplitMe'), {
+    fallback: <div>loading...</div>
+});
+// loadable을 사용하는 부분을 수정하여 로딩 중에 다른 UI를 보여줌
 
 function App() {
+  const [visible, setVisible] = useState(false);
   const onClick = () => {
-    import("./notify").then((result) => result.default());  //import 대신 import 함수를 사용
+    setVisible(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
   };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+           Hello React!
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
